d9bb33d780c0a03f91c67d67e132cd9a74de2745
UW Muon Lab <uw.muon.dropbox@gmail.com>
2016-01-26 16:32:26 -0800

    removed which loops in favor of static visitors

--- src/worker_list.cxx
+++ src/worker_list.cxx
@@ -3,411 +3,208 @@
 
 namespace daq {
 
-void WorkerList::StartRun()
-{
+void WorkerList::StartRun() {
   StartThreads();
   StartWorkers();
 }
 
-void WorkerList::StopRun()
-{
+void WorkerList::StopRun() {
   StopWorkers();
   StopThreads();
 }
 
-void WorkerList::StartWorkers()
-{
+struct StartWorkersVis : public boost::static_visitor<> {
+  template <typename T>
+  void operator()(T workerptr) {
+    workerptr->StartWorker();
+  }
+};
+
+void WorkerList::StartWorkers() {
   // Starts gathering data.
   LogMessage("Starting workers");
 
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
-
-    // Needs a case switch to work with boost::variant vector.
-    if ((*it).which() == 0) {
-
-      boost::get<WorkerBase<sis_3350> *>(*it)->StartWorker();
-
-    } else if ((*it).which() == 1) {
-
-      boost::get<WorkerBase<sis_3302> *>(*it)->StartWorker();
-
-    } else if ((*it).which() == 2) {
-
-      boost::get<WorkerBase<caen_1785> *>(*it)->StartWorker();
-
-    } else if ((*it).which() == 3) {
-
-      boost::get<WorkerBase<caen_6742> *>(*it)->StartWorker();
-
-    } else if ((*it).which() == 4) {
-
-      boost::get<WorkerBase<drs4> *>(*it)->StartWorker();
-
-    } else if ((*it).which() == 5) {
-
-      boost::get<WorkerBase<caen_1742> *>(*it)->StartWorker();
-
-    } else if ((*it).which() == 6) {
-
-      boost::get<WorkerBase<sis_3316> *>(*it)->StartWorker();
-    }
+  StartWorkersVis vis;
+  for (auto &workervar : workers_) {
+    boost::apply_visitor(vis, workervar);
   }
 }
 
-void WorkerList::StartThreads()
-{
+struct StartThreadsVis : public boost::static_visitor<> {
+  template <typename T>
+  void operator()(T workerptr) {
+    workerptr->StartThread();
+  }
+};
+
+void WorkerList::StartThreads() {
   // Launches the data worker threads.
   LogMessage("Launching worker threads");
 
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
-
-    if ((*it).which() == 0) {
-
-      boost::get<WorkerBase<sis_3350> *>(*it)->StartThread();
-
-    } else if ((*it).which() == 1) {
-
-      boost::get<WorkerBase<sis_3302> *>(*it)->StartThread();
-
-    } else if ((*it).which() == 2) {
-
-      boost::get<WorkerBase<caen_1785> *>(*it)->StartThread();
-
-    } else if ((*it).which() == 3) {
-
-      boost::get<WorkerBase<caen_6742> *>(*it)->StartThread();
-
-    } else if ((*it).which() == 4) {
-
-      boost::get<WorkerBase<drs4> *>(*it)->StartThread();
-
-    } else if ((*it).which() == 5) {
-
-      boost::get<WorkerBase<caen_1742> *>(*it)->StartThread();
-
-    } else if ((*it).which() == 6) {
-
-      boost::get<WorkerBase<sis_3316> *>(*it)->StartThread();
-    }
+  StartThreadsVis vis;
+  for (auto &workervar : workers_) {
+    boost::apply_visitor(vis, workervar);
   }
 }
 
-void WorkerList::StopWorkers()
-{
+struct StopWorkersVis : public boost::static_visitor<> {
+  template <typename T>
+  void operator()(T workerptr) {
+    workerptr->StopWorker();
+  }
+};
+
+void WorkerList::StopWorkers() {
   // Stop collecting data.
   LogMessage("Stopping workers");
 
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
-
-    if ((*it).which() == 0) {
-
-      boost::get<WorkerBase<sis_3350> *>(*it)->StopWorker();
-
-    } else if ((*it).which() == 1) {
-
-      boost::get<WorkerBase<sis_3302> *>(*it)->StopWorker();
-
-    } else if ((*it).which() == 2) {
-
-      boost::get<WorkerBase<caen_1785> *>(*it)->StopWorker();
-
-    } else if ((*it).which() == 3) {
-
-      boost::get<WorkerBase<caen_6742> *>(*it)->StopWorker();
-
-    } else if ((*it).which() == 4) {
-
-      boost::get<WorkerBase<drs4> *>(*it)->StopWorker();
-
-    } else if ((*it).which() == 5) {
-
-      boost::get<WorkerBase<caen_1742> *>(*it)->StopWorker();
-
-    } else if ((*it).which() == 6) {
-
-      boost::get<WorkerBase<sis_3316> *>(*it)->StopWorker();
-    }
+  StopWorkersVis vis;
+  for (auto &workervar : workers_) {
+    boost::apply_visitor(vis, workervar);
   }
 }
 
-void WorkerList::StopThreads()
-{
+struct StopThreadsVis : public boost::static_visitor<> {
+  template <typename T>
+  void operator()(T workerptr) {
+    workerptr->StopThread();
+  }
+};
+
+void WorkerList::StopThreads() {
   // Stop and rejoin worker threads.
   LogMessage("Stopping worker threads");
 
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
-
-    if ((*it).which() == 0) {
-
-      boost::get<WorkerBase<sis_3350> *>(*it)->StopThread();
-
-    } else if ((*it).which() == 1) {
-
-      boost::get<WorkerBase<sis_3302> *>(*it)->StopThread();
-
-    } else if ((*it).which() == 2) {
-
-      boost::get<WorkerBase<caen_1785> *>(*it)->StopThread();
-
-    } else if ((*it).which() == 3) {
-
-      boost::get<WorkerBase<caen_6742> *>(*it)->StopThread();
-
-    } else if ((*it).which() == 4) {
-
-      boost::get<WorkerBase<drs4> *>(*it)->StopThread();
-
-    } else if ((*it).which() == 5) {
-
-      boost::get<WorkerBase<caen_1742> *>(*it)->StopThread();
-
-    } else if ((*it).which() == 6) {
-
-      boost::get<WorkerBase<sis_3316> *>(*it)->StopThread();
-    }
+  StopThreadsVis vis;
+  for (auto &workervar : workers_) {
+    boost::apply_visitor(vis, workervar);
   }
 }
 
-bool WorkerList::AllWorkersHaveEvent()
-{
+struct HasEventVis : public boost::static_visitor<bool> {
+  template <typename T>
+  bool operator()(T workerptr) {
+    return workerptr->HasEvent();
+  }
+};
+
+bool WorkerList::AllWorkersHaveEvent() {
   // Check each worker for an event.
   bool has_event = true;
 
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
-
-    // If any worker doesn't have an event, has_event will become false.
-    if ((*it).which() == 0) {
-
-      has_event &= boost::get<WorkerBase<sis_3350> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 1) {
-
-      has_event &= boost::get<WorkerBase<sis_3302> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 2) {
-
-      has_event &= boost::get<WorkerBase<caen_1785> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 3) {
-
-      has_event &= boost::get<WorkerBase<caen_6742> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 4) {
-
-      has_event &= boost::get<WorkerBase<drs4> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 5) {
-
-      has_event &= boost::get<WorkerBase<caen_1742> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 6) {
-
-      has_event &= boost::get<WorkerBase<sis_3316> *>(*it)->HasEvent();
-    }
+  HasEventVis vis;
+  for (auto &workervar : workers_) {
+    has_event &= apply_visitor(vis, workervar);
   }
 
   return has_event;
 }
 
-bool WorkerList::AnyWorkersHaveEvent()
-{
+bool WorkerList::AnyWorkersHaveEvent() {
   // Check each worker for an event.
   bool any_events = false;
 
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
-
-    // A bitwise or here works, so that any event will return positive.
-    if ((*it).which() == 0) {
-
-      any_events |= boost::get<WorkerBase<sis_3350> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 1) {
-
-      any_events |= boost::get<WorkerBase<sis_3302> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 2) {
-
-      any_events |= boost::get<WorkerBase<caen_1785> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 3) {
-
-      any_events |= boost::get<WorkerBase<caen_6742> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 4) {
-
-      any_events |= boost::get<WorkerBase<drs4> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 5) {
-
-      any_events |= boost::get<WorkerBase<caen_1742> *>(*it)->HasEvent();
-
-    } else if ((*it).which() == 6) {
-
-      any_events |= boost::get<WorkerBase<sis_3316> *>(*it)->HasEvent();
-    }
+  HasEventVis vis;
+  for (auto &workervar : workers_) {
+    any_events |= apply_visitor(vis, workervar);
   }
 
   return any_events;
 }
 
-bool WorkerList::AnyWorkersHaveMultiEvent()
-{
+struct NumEventsVis : public boost::static_visitor<int> {
+  template <typename T>
+  int operator()(T workerptr) {
+    return workerptr->num_events();
+  }
+};
+
+bool WorkerList::AnyWorkersHaveMultiEvent() {
   // Check each worker for more than one event.
-  int num_events = 0;
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
-
-    if ((*it).which() == 0) {
-
-      num_events = boost::get<WorkerBase<sis_3350> *>(*it)->num_events();
-
-    } else if ((*it).which() == 1) {
-
-      num_events = boost::get<WorkerBase<sis_3302> *>(*it)->num_events();
-
-    } else if ((*it).which() == 2) {
-
-      num_events = boost::get<WorkerBase<caen_1785> *>(*it)->num_events();
-
-    } else if ((*it).which() == 3) {
-
-      num_events = boost::get<WorkerBase<caen_6742> *>(*it)->num_events();
-
-    } else if ((*it).which() == 4) {
-
-      num_events = boost::get<WorkerBase<drs4> *>(*it)->num_events();
-
-    } else if ((*it).which() == 5) {
-
-      num_events = boost::get<WorkerBase<caen_1742> *>(*it)->num_events();
-
-    } else if ((*it).which() == 6) {
-
-      num_events = boost::get<WorkerBase<sis_3316> *>(*it)->num_events();
+  NumEventsVis vis;
+  for (auto &workervar : workers_) {
+    if (apply_visitor(vis, workervar) > 1) {
+      return true;
     }
-
-    if (num_events > 1) return true;
   }
 
   return false;
 }
 
-void WorkerList::GetEventData(event_data &bundle)
-{
-  // Loops over each worker and collect the event data.
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
+struct GetEventDataVis : public boost::static_visitor<> {
+  void operator()(WorkerBase<sis_3350> *workerptr) {
+    bundle->sis_3350_vec.push_back(workerptr->PopEvent());
+  }
 
-    if ((*it).which() == 0) {
+  void operator()(WorkerBase<sis_3302> *workerptr) {
+    bundle->sis_3302_vec.push_back(workerptr->PopEvent());
+  }
 
-      auto ptr = boost::get<WorkerBase<sis_3350> *>(*it);
-      bundle.sis_3350_vec.push_back(ptr->PopEvent());
+  void operator()(WorkerBase<caen_1785> *workerptr) {
+    bundle->caen_1785_vec.push_back(workerptr->PopEvent());
+  }
 
-    } else if ((*it).which() == 1) {
+  void operator()(WorkerBase<caen_6742> *workerptr) {
+    bundle->caen_6742_vec.push_back(workerptr->PopEvent());
+  }
 
-      auto ptr = boost::get<WorkerBase<sis_3302> *>(*it);
-      bundle.sis_3302_vec.push_back(ptr->PopEvent());
+  void operator()(WorkerBase<drs4> *workerptr) {
+    bundle->drs4_vec.push_back(workerptr->PopEvent());
+  }
 
-    } else if ((*it).which() == 2) {
+  void operator()(WorkerBase<caen_1742> *workerptr) {
+    bundle->caen_1742_vec.push_back(workerptr->PopEvent());
+  }
 
-      auto ptr = boost::get<WorkerBase<caen_1785> *>(*it);
-      bundle.caen_1785_vec.push_back(ptr->PopEvent());
+  void operator()(WorkerBase<sis_3316> *workerptr) {
+    bundle->sis_3316_vec.push_back(workerptr->PopEvent());
+  }
 
-    } else if ((*it).which() == 3) {
+  event_data *bundle;
+};
 
-      auto ptr = boost::get<WorkerBase<caen_6742> *>(*it);
-      bundle.caen_6742_vec.push_back(ptr->PopEvent());
+void WorkerList::GetEventData(event_data &bundle) {
+  GetEventDataVis vis;
+  vis.bundle = &bundle;
 
-    } else if ((*it).which() == 4) {
-
-      auto ptr = boost::get<WorkerBase<drs4> *>(*it);
-      bundle.drs4_vec.push_back(ptr->PopEvent());
-
-    } else if ((*it).which() == 5) {
-
-      auto ptr = boost::get<WorkerBase<caen_1742> *>(*it);
-      bundle.caen_1742_vec.push_back(ptr->PopEvent());
-
-    } else if ((*it).which() == 6) {
-
-      auto ptr = boost::get<WorkerBase<sis_3316> *>(*it);
-      bundle.sis_3316_vec.push_back(ptr->PopEvent());
-    }
+  for (auto &workervar : workers_) {
+    boost::apply_visitor(vis, workervar);
   }
 }
 
-void WorkerList::FlushEventData()
-{
+struct FlushEventDataVis : public boost::static_visitor<> {
+  template <typename T>
+  void operator()(T workerptr) {
+    workerptr->FlushEvents();
+  }
+};
+
+void WorkerList::FlushEventData() {
   // Drops any stale events when workers should have no events.
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {
 
-    if ((*it).which() == 0) {
-
-      boost::get<WorkerBase<sis_3350> *>(*it)->FlushEvents();
-
-    } else if ((*it).which() == 1) {
-
-      boost::get<WorkerBase<sis_3302> *>(*it)->FlushEvents();
-
-    } else if ((*it).which() == 2) {
-
-      boost::get<WorkerBase<caen_1785> *>(*it)->FlushEvents();
-
-    } else if ((*it).which() == 3) {
-
-      boost::get<WorkerBase<caen_6742> *>(*it)->FlushEvents();
-
-    } else if ((*it).which() == 4) {
-
-      boost::get<WorkerBase<drs4> *>(*it)->FlushEvents();
-
-    } else if ((*it).which() == 5) {
-
-      boost::get<WorkerBase<caen_1742> *>(*it)->FlushEvents();
-
-    } else if ((*it).which() == 6) {
-
-      boost::get<WorkerBase<sis_3316> *>(*it)->FlushEvents();
-    }
-  } 
+  FlushEventDataVis vis;
+  for (auto &workervar : workers_) {
+    boost::apply_visitor(vis, workervar);
+  }
 }
 
-void WorkerList::FreeList()
-{
+struct FreeListVis : boost::static_visitor<> {
+  template <typename T>
+  void operator()(T workerptr) {
+    delete workerptr;
+  }
+};
+
+void WorkerList::FreeList() {
   // Delete the allocated workers.
   LogMessage("Freeing workers");
 
-  for (auto it = workers_.begin(); it != workers_.end(); ++it) {       
-
-    if ((*it).which() == 0) {
-
-      delete boost::get<WorkerBase<sis_3350> *>(*it);
-
-    } else if ((*it).which() == 1) {
-
-      delete boost::get<WorkerBase<sis_3302> *>(*it);
-
-    } else if ((*it).which() == 2) {
-
-      delete boost::get<WorkerBase<caen_1785> *>(*it);
-
-    } else if ((*it).which() == 3) {
-
-      delete boost::get<WorkerBase<caen_6742> *>(*it);
-
-    } else if ((*it).which() == 4) {
-
-      delete boost::get<WorkerBase<drs4> *>(*it);
-
-    } else if ((*it).which() == 5) {
-
-      delete boost::get<WorkerBase<caen_1742> *>(*it);
-
-    } else if ((*it).which() == 6) {
-
-      delete boost::get<WorkerBase<sis_3316> *>(*it);
-    }
+  FreeListVis vis;
+  for (auto &workervar : workers_) {
+    boost::apply_visitor(vis, workervar);
   }
 
   Resize(0);
 }
 
-} // ::daq
+}  // ::daq
